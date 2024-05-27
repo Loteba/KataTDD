ebd96ce5541739acaee900eb870516ca84869588
loteba <74804104@continental.edu.pe>
2024-05-27 15:07:41 -0500

    Caso 4: Calcular promedio con un conjunto de n elementos (Verde)

--- tests/TestConjunto.py
+++ tests/TestConjunto.py
@@ -20,12 +20,15 @@ class TestConjunto( unittest.TestCase ):
 
 
 if __name__== "__main__"
-    def promedio( self ):
+    def promedio(self):
         if len(self.__conjunto) == 1:
             return (self.__conjunto[0])
         elif len(self.__conjunto) == 2:
-            return (self.__conjunto[0]+self.__conjunto[1])/2
+            return (self.__conjunto[0] + self.__conjunto[1]) / 2
+        elif len(self.__conjunto) > 2:
+            return sum(self.__conjunto) / len(self.__conjunto)
         else:
             return None
+
 
 
