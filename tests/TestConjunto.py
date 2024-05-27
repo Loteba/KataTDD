c53fd79d5e657d90b8e43684b1aad3249eab19f7
loteba <74804104@continental.edu.pe>
2024-05-27 15:11:31 -0500

    Caso 4: Calcular promedio con un conjunto de n elementos (Azul)

--- tests/TestConjunto.py
+++ tests/TestConjunto.py
@@ -31,4 +31,13 @@ if __name__== "__main__"
             return None
 
 
+class Conjunto:
+    def __init__(self, conjunto):
+        self.__conjunto=conjunto
+
+    def promedio( self ):
+        if len(self.__conjunto) > 0:
+            return sum(self.__conjunto)/len(self.__conjunto)
+        else:
+            return None
 
