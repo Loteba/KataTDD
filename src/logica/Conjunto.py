fbe1111df208b09b9290418e80d513ed109c7b96
Loteba <74804104@continetal.edu.pe>
2024-06-03 13:48:27 -0500

    Commit changes in submodule

--- src/logica/Conjunto.py
+++ src/logica/Conjunto.py
@@ -1,8 +1,13 @@
 
 class Conjunto:
-    def __init__(self, conjunto):
-        self.__conjunto=conjunto
+    def __init__(self, datos, pesos):
+        self.__datos = datos
+        self.__pesos = pesos
 
-    def promedio( self ):
-        return False
-
+    def promedio(self):
+        if len(self.__datos) == 0 or len(self.__pesos) == 0 or len(self.__datos) != len(self.__pesos):
+            return None
+        else:
+            total_peso = sum(self.__pesos)
+            promedio_ponderado = sum(d * p for d, p in zip(self.__datos, self.__pesos)) / total_peso
+            return promedio_ponderado
--- tests/TestConjunto.py
+++ tests/TestConjunto.py
@@ -1,43 +1,36 @@
 import unittest
-import src.logica
+import sys
+import os
+
+# Añadir el directorio base del proyecto al sys.path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from src.logica.Conjunto import Conjunto
 
-class TestConjunto( unittest.TestCase ):
-    def test_conjunto_vacio_retornaNone( self ):
-        conjunto=Conjunto([])
+class TestConjunto(unittest.TestCase):
+    def test_conjunto_vacio_retornaNone(self):
+        conjunto = Conjunto([], [])
         self.assertIsNone(conjunto.promedio())
 
-    def test_conjunto_unElemento_retornaValorUnicoElemento( self ):
-        conjunto=Conjunto([5])
-        self.assertEqual(5,conjunto.promedio())
+    def test_conjunto_unElemento_retornaValorUnicoElemento(self):
+        conjunto = Conjunto([5], [1])
+        self.assertEqual(5, conjunto.promedio())
 
-    def test_conjunto_dosElementos_retornaPromedioElementos( self ):
-        conjunto=Conjunto([5,7])
-        self.assertEqual(6,conjunto.promedio())
-    def test_conjunto_nElementos_retornaPromedioNElementos( self ):
-        conjunto=Conjunto([2,4,8,9,10,15])
-        self.assertEqual((2+4+8+9+10+15)/6,conjunto.promedio())
+    def test_conjunto_dosElementos_retornaPromedioElementos(self):
+        conjunto = Conjunto([5, 7], [1, 1])
+        self.assertEqual(6, conjunto.promedio())
 
+    def test_conjunto_nElementos_retornaPromedioNElementos(self):
+        conjunto = Conjunto([2, 4, 8, 9, 10, 15], [1, 1, 1, 1, 1, 1])
+        self.assertEqual((2 + 4 + 8 + 9 + 10 + 15) / 6, conjunto.promedio())
 
-if __name__== "__main__"
-    def promedio(self):
-        if len(self.__conjunto) == 1:
-            return (self.__conjunto[0])
-        elif len(self.__conjunto) == 2:
-            return (self.__conjunto[0] + self.__conjunto[1]) / 2
-        elif len(self.__conjunto) > 2:
-            return sum(self.__conjunto) / len(self.__conjunto)
-        else:
-            return None
+    def test_conjunto_datosPesosEjemplo0(self):
+        conjunto = Conjunto([10, 12, 14], [3, 4, 2])
+        self.assertAlmostEqual(11.78, conjunto.promedio(), places=2)
 
+    def test_conjunto_datosPesosEjemplo1(self):
+        conjunto = Conjunto([15, 15, 17], [3, 4, 2])
+        self.assertAlmostEqual(15.44, conjunto.promedio(), places=2)
 
-class Conjunto:
-    def __init__(self, conjunto):
-        self.__conjunto=conjunto
-
-    def promedio( self ):
-        if len(self.__conjunto) > 0:
-            return sum(self.__conjunto)/len(self.__conjunto)
-        else:
-            return None
-
+if __name__ == "__main__":
+    unittest.main()
